247b8632fdefe20d1bfe8a426e1aba1c796ca955
hemansutanty <hemansu.tanty@gmail.com>
2016-04-23 01:23:38 +0530

    Changed url pattern of controller

--- src/com/githubanalysis/controller/UserController.java
+++ src/com/githubanalysis/controller/UserController.java
@@ -14,7 +14,7 @@ import com.githubanalysis.model.User;
 /**
  * Servlet implementation class UserController
  */
-@WebServlet("/UserController")
+@WebServlet("/*UserController")
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
